fcdb06d3ae7260c86c4fc8a4901019f37f4779ad
Tan <ngoctan26@gmail.com>
2017-03-13 23:41:37 +0700

    Update services folder

---
+++ src/ParkHere/ParkHere/Services/FirebaseClient.swift
@@ -0,0 +1,13 @@
+//
+//  FirebaseClient.swift
+//  ParkHere
+//
+//  Created by Nguyen Quang Ngoc Tan on 3/13/17.
+//  Copyright © 2017 Nguyen Quang Ngoc Tan. All rights reserved.
+//
+
+import Foundation
+
+class FirebaseClient {
+    
+}
