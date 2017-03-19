ba6cb1623328be9e45c1809eb6ea67548ab014e4
Tan <ngoctan26@gmail.com>
2017-03-19 14:25:10 +0700

    Update model gmaproute

--- src/ParkHere/ParkHere/Model/GMapRoute.swift
+++ src/ParkHere/ParkHere/Model/GMapRoute.swift
@@ -11,14 +11,28 @@ import CoreLocation
 
 class GMapRoute {
     var routeAsPoints: String!
+    var totalDistance: Int! // In meters
+    var totalDuration: Int! // In seconds
     
     init (routeResponse: [String: AnyObject]) {
         let overViewPolyline = routeResponse["overview_polyline"] as! Dictionary<String, AnyObject>
         routeAsPoints = overViewPolyline["points"] as! String
         let legs = routeResponse["legs"] as! Array<Dictionary<String, AnyObject>>
-        let startLocationDictionary = legs[0]["start_location"] as! Dictionary<String, AnyObject>
-        let originCoordinate = CLLocationCoordinate2D(latitude: startLocationDictionary["lat"] as! Double, longitude: startLocationDictionary["lng"] as! Double)
-        let endLocationDictionary = legs[legs.count - 1]["end_location"] as! Dictionary<String, AnyObject>
-        let endCoordinate = CLLocationCoordinate2D(latitude: endLocationDictionary["lat"] as! Double, longitude: endLocationDictionary["lng"] as! Double)
+        calulateDistanceAndDuration(legs: legs)
+    }
+    
+    private func calulateDistanceAndDuration(legs: [[String : AnyObject]]) {
+        var totalDistanceInMeters = 0
+        var totalDurationInSeconds = 0
+        for leg in legs {
+            totalDistanceInMeters += (leg["distance"] as! Dictionary<String, AnyObject>)["value"] as! Int
+            totalDurationInSeconds += (leg["duration"] as! Dictionary<String, AnyObject>)["value"] as! Int
+        }
+        totalDistance = totalDistanceInMeters
+        totalDuration = totalDurationInSeconds
+    }
+    
+    func getDistanceInKm() -> Double {
+        return Double(totalDistance / 1000)
     }
 }
